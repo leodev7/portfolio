b41b28092e7211d0b2cf7db2092b137e308440d3
Leonardo Dev <leonard296@live.com>
2020-06-22 21:20:10 -0300

    update css

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+const pegarClasse = document.querySelectorAll('.habilidades li a');
+
+const pegarImagem = document.querySelectorAll('.habilidades img');
+
+pegarClasse.forEach(pegarClasse => {
+    pegarClasse.addEventListener('click', function(event) {
+       if (pegarClasse.classList.value === ''){
+             pegarClasse.classList.add('ativo');
+        } else if (pegarClasse.classList.value != ''){
+            pegarClasse.classList.remove('ativo');
+        }
+    });
+});
+
+pegarImagem.forEach(pegarImagem => {
+    pegarImagem.addEventListener('click', function(event) {
+       if (pegarImagem.classList.value === ''){
+             pegarImagem.classList.add('ativo_img');
+        } else if (pegarImagem.classList.value != ''){
+            pegarImagem.classList.remove('ativo_img');
+        }
+    });
+});
