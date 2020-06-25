9cbb9f11382d97783e94d0fa3ec250445bdf0a23
Leonardo Dev <leonard296@live.com>
2020-06-25 09:38:15 -0300

    update sobre-mim

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
-const linksInternos = document.querySelectorAll('.habilidades li a');
 
-const pegarImagem = document.querySelector('.habilidades img');
+// function handleLink(event){
+// 	event.preventDefault();
+//     linksInternos.forEach((item) => {
+// 		item.classList.remove('ativo');
+// 	})
+// 	event.currentTarget.classList.add('ativo');
+// }
 
-// pegarImagem.setAttribute("src", "img/investidor.jpg")
+// linksInternos.forEach((link) => {
+//     link.addEventListener('click', handleLink)
+// })
+
+const tabLink = document.querySelectorAll('.js-tabLink li a');
+const tabImagens = document.querySelectorAll('.js-tabImagens li');
 
 
+if (tabLink.length && tabImagens.length) {
+	tabImagens[0].classList.add('ativo')
+	tabLink[0].classList.add('ativoLink')
+    function activeTab(index) {
+        tabImagens.forEach((section) => {
+            section.classList.remove('ativo')
+		})
+		tabLink.forEach((section) => {
+			section.classList.remove('ativoLink')
+		})
+		tabImagens[index].classList.add('ativo')
+		tabLink[index].classList.add('ativoLink')
+    }
 
-function handleLink(event){
-	event.preventDefault();
-    linksInternos.forEach((item) => {
-		item.classList.remove('ativo');
-	})
-	event.currentTarget.classList.add('ativo');
+    tabLink.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        })
+    })
 }
-
-linksInternos.forEach((link) => {
-    link.addEventListener('click', handleLink)
-})
