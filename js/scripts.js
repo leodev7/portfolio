f294bc4d467aa4d150826518222108de8c4ebe9b
Leonardo Dev <leonard296@live.com>
2020-07-05 10:46:29 -0300

    add text sobre-mim

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var myButton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -16,26 +18,33 @@ function topFunction() {
 }
 
 const tabLink = document.querySelectorAll('.js-tabLink li a');
+const tabText = document.querySelectorAll('.js-tabLink p');
 const tabImagens = document.querySelectorAll('.js-tabImagens li');
 
-
 if (tabLink.length && tabImagens.length) {
-	tabImagens[0].classList.add('ativo')
-	tabLink[0].classList.add('ativoLink')
-    function activeTab(index) {
-        tabImagens.forEach((section) => {
-            section.classList.remove('ativo')
-		})
-		tabLink.forEach((section) => {
-			section.classList.remove('ativoLink')
-		})
-		tabImagens[index].classList.add('ativo')
-		tabLink[index].classList.add('ativoLink')
-    }
+  tabLink[0].classList.add('ativoLink');
+  tabText[0].classList.add('ativo');
+  tabImagens[0].classList.add('ativo');
 
-    tabLink.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        })
+  function activeTab(index) {
+    tabLink.forEach((section) => {
+      section.classList.remove('ativoLink');
     })
+    tabText.forEach((section) => {
+      section.classList.remove('ativo');
+    })
+    tabImagens.forEach((section) => {
+      section.classList.remove('ativo');
+    })
+
+    tabLink[index].classList.add('ativoLink');
+    tabText[index].classList.add('ativo');
+    tabImagens[index].classList.add('ativo');
+  }
+
+  tabLink.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    })
+  })
 }
