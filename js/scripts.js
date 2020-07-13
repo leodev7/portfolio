0f64d80d9abf5a6133cc4db72d78d7ac91adff07
Leonardo Dev <leonard296@live.com>
2020-07-13 16:49:32 -0300

    SEO and scroll animation

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,24 @@ if (tabLink.length && tabImagens.length) {
     })
   })
 }
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if (sections.length) {
+      const windowMetade = window.innerHeight * 0.6;
+
+      function animaScroll() {
+          sections.forEach((section) => {
+              const sectionTop = section.getBoundingClientRect().top - windowMetade;
+              if (sectionTop < 0)
+                  section.classList.add('ativo')
+              else
+                  section.classList.remove('ativo')
+          })
+      }
+      animaScroll();
+      window.addEventListener('scroll', animaScroll)
+  }
+}
+
+initAnimacaoScroll();
